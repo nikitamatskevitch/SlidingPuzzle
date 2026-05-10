7f92514cceec5c7a2e72f60aea1afbac76c40b9c
nickworker1068 <nickworker1068@gmail.com>
2026-05-10 22:27:34 +0300

    1.0

---
+++ Assets/DTT/Minigame - SlidingPuzzle/Runtime/MiniGame/SlidingPuzzleLevelSelectHandler.cs
@@ -0,0 +1,38 @@
+using DTT.MinigameBase.UI;
+using UnityEngine;
+using DTT.MinigameBase.LevelSelect;
+
+namespace DTT.MiniGame.SlidingPuzzle
+{
+    public class SlidingPuzzleLevelSelectHandler : LevelSelectHandler<LevelData, SlidingPuzzleResult, SlidingPuzzleManager>
+    {
+        [SerializeField] private LevelData[] _configs;
+
+        private void Start()
+        {
+            GameUI gameUI = FindObjectOfType<GameUI>();
+            if (gameUI != null)
+                gameUI.NextLevelRequested += LoadNextLevel;
+        }
+
+        private void LoadNextLevel()
+        {
+            int nextLevelNumber = CurrentLevel + 1;
+            if (nextLevelNumber > _configs.Length)
+            {
+                ShowLevelSelect();
+                return;
+            }
+
+            var minigame = FindObjectOfType<SlidingPuzzleManager>();
+            if (minigame == null) return;
+
+            minigame.ClearLevel();
+            minigame.StartGame(GetConfig(nextLevelNumber));
+        }
+
+        protected override float CalculateScore(SlidingPuzzleResult result) => result.score;
+
+        protected override LevelData GetConfig(int levelNumber) => _configs[(levelNumber - 1) % _configs.Length];
+    }
+}
