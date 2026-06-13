195cd2afec9450b5f19f8c99213754193376b28a
nikitamatskevitch <74095447+nikitamatskevitch@users.noreply.github.com>
2026-06-13 22:39:34 +0300

    Fix autocomplete reward ad flow

---
+++ Assets/PluginYourGames/Modules/RewardedAdv/Scripts/Editor/CopyCode/RewardedAdv_js.js
@@ -0,0 +1,47 @@
+﻿function RewardedAdvShow(id) {
+    try {
+        if (ysdk == null) {
+            LogStyledMessage('Cancel RewardedAdvShow: SDK is not initialized');
+            return;
+        }
+        if (nowFullAdOpen == true) {
+            LogStyledMessage('Cancel RewardedAdvShow: The advertisement is already open');
+            return;
+        }
+
+        ysdk.adv.showRewardedVideo({
+            callbacks: {
+                onOpen: () => {
+                    LogStyledMessage('Open Rewarded Adv');
+                    nowFullAdOpen = true;
+                    if (initGame === true) {
+                        YG2Instance('OpenRewardedAdv');
+                    }
+                },
+                onRewarded: () => {
+                    LogStyledMessage('Rewarded Adv reward');
+                    if (initGame === true) {
+                        YG2Instance('RewardAdv', id);
+                    }
+                },
+                onClose: () => {
+                    LogStyledMessage('Close Rewarded Adv');
+                    nowFullAdOpen = false;
+                    if (initGame === true) {
+                        YG2Instance('CloseRewardedAdv');
+                    }
+                    FocusGame();
+                },
+                onError: (error) => {
+                    console.error('Error Rewarded Adv', error);
+                    nowFullAdOpen = false;
+                    YG2Instance('ErrorRewardedAdv');
+                    FocusGame();
+                }
+            }
+        });
+    }
+    catch (e) {
+        console.error('CRASH Rewarded Adv Show: ', e.message);
+    }
+}
---
+++ Assets/PluginYourGames/Modules/RewardedAdv/Scripts/RewardedAdv_yg.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using YG.Insides;
+using UnityEngine;
+
+namespace YG
+{
+    public static partial class YG2
+    {
+        public static Action onOpenRewardedAdv;
+        public static Action onCloseRewardedAdv;
+        public static Action<string> onRewardAdv;
+        public static Action onErrorRewardedAdv;
+
+        [InitYG]
+        private static void InitRewardedAdv()
+        {
+#if UNITY_EDITOR
+            onOpenRewardedAdv = null;
+            onCloseRewardedAdv = null;
+            onRewardAdv = null;
+            onErrorRewardedAdv = null;
+#endif
+        }
+
+        public static void RewardedAdvShow(string id)
+        {
+#if UNITY_EDITOR
+            if (infoYG.Simulation.testFailAds)
+            {
+                Message("Error Rewarded Adv simulation");
+                YGInsides.ErrorRewardedAdv();
+                return;
+            }
+
+            AdvCallingSimulation.RewardedAdvOpen(id, infoYG.Simulation.durationAdv);
+#else
+            if (nowAdsShow)
+            {
+#if RU_YG2
+                Message("Реклама уже открыта");
+#else
+                Message("The advertisement is already open");
+#endif
+                return;
+            }
+
+            Message("Rewarded Adv");
+            iPlatform.RewardedAdvShow(id);
+#endif
+        }
+    }
+}
+
+namespace YG.Insides
+{
+    public static partial class YGInsides
+    {
+        public static void OpenRewardedAdv()
+        {
+            YG2.PauseGame(true);
+            YG2.onOpenRewardedAdv?.Invoke();
+            YG2.onOpenAnyAdv?.Invoke();
+            YG2.nowRewardAdv = true;
+        }
+
+        public static void CloseRewardedAdv()
+        {
+            YG2.nowRewardAdv = false;
+            YG2.onCloseRewardedAdv?.Invoke();
+            YG2.onCloseAnyAdv?.Invoke();
+            YG2.PauseGame(false);
+        }
+
+        public static void RewardAdv(string id)
+        {
+            YG2.onRewardAdv?.Invoke(id);
+        }
+
+        public static void ErrorRewardedAdv()
+        {
+            YG2.nowRewardAdv = false;
+            YG2.onErrorRewardedAdv?.Invoke();
+            YG2.onErrorAnyAdv?.Invoke();
+            YG2.PauseGame(false);
+        }
+    }
+}
