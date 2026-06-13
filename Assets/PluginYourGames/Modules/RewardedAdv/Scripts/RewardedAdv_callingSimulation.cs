52fc120d5af0cefcf93b131f510477f74ede6616
nikitamatskevitch <74095447+nikitamatskevitch@users.noreply.github.com>
2026-06-13 22:39:34 +0300

    Fix autocomplete reward ad flow

---
+++ Assets/PluginYourGames/Modules/RewardedAdv/Scripts/RewardedAdv_callingSimulation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using YG.Insides;
+
+namespace YG
+{
+    public partial class AdvCallingSimulation
+    {
+        public static void RewardedAdvOpen(string id, float duration)
+        {
+            AdvCallingSimulation call = CreateCallSimulation();
+            call.StartCoroutine(call.RewardedAdvOpenCoroutine(id, duration));
+        }
+
+        private IEnumerator RewardedAdvOpenCoroutine(string id, float duration)
+        {
+            yield return new WaitForSecondsRealtime(YG2.infoYG.Simulation.loadAdv);
+            YGInsides.OpenRewardedAdv();
+            yield return new WaitForSecondsRealtime(duration);
+            YGInsides.RewardAdv(id);
+            YGInsides.CloseRewardedAdv();
+            Destroy(gameObject);
+        }
+    }
+}
